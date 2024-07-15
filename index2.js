573928225ba53321a9fd85422846b7c5385ae9a2
jon-jh <hiebert456@gmail.com>
2024-07-15 16:27:20 -0600

    finish iss promise version and index2 which calls it

--- index2.js
+++ index2.js
@@ -1,4 +1,15 @@
-const { getIP } = require('./iss_promised');
+// const { getIP, getCoords, flyoverT } = require('./iss_promised');
+const { ISSflyoverT} = require('./iss_promised');
+// // Testing
+// getIP()
+//   .then((response) => getCoords(response))
+//   .then((coords) => flyoverT(coords))
+//   .then(body => console.log(body));
 
-getIP()
-  .then((response) => console.log(response));
+// Final
+
+ISSflyoverT();
+
+// Note: error handling (.catch) not added
+// time formatter not added
+// maybe in the future when I have time to come back to it
--- iss_promised.js
+++ iss_promised.js
@@ -9,6 +9,7 @@ const getIP = function() {
     .then((response) => {
       const body = response.body;
       const ip = body.ip;
+      console.log(ip);
       return ip;
     // console.log(body) // prints object with ip: x.x.x.x
     // console.log(ip) // prints user ip
@@ -18,4 +19,41 @@ const getIP = function() {
 //Test
 // getIP()
 
-module.exports = { getIP };
+const getCoords = function(ip) { //remember this function needs to take the up from the previous function, here.
+  let url = `http://ipwho.is/${ip}`;
+  return needle('get', url)
+
+    .then((response) => {
+      const body = response.body;
+      const lat = body.latitude;
+      const long = body.longitude;
+      console.log({lat, long});
+      return {lat, long};
+    });
+};
+
+const flyoverT = function(coords) {
+  const latitude = coords.lat;
+  const longitude = coords.long;
+  const url = `https://iss-flyover.herokuapp.com/json/?lat=${latitude}&lon=${longitude}`;
+  return needle('get', url)
+    .then((response) => {
+      const body = response.body;
+      const passtimes = body.response;
+      return passtimes;
+    });
+};
+
+
+const ISSflyoverT = function() {
+  return getIP()
+    .then((ip) => getCoords(ip))
+    .then((coords) => flyoverT(coords))
+    .then((passtimes) => {
+      console.log(passtimes);
+      return passtimes;
+    });
+};
+
+module.exports = { ISSflyoverT };
+// module.exports = { getIP, getCoords, flyoverT };
